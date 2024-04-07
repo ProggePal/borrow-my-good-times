213b06a58e4859a7a96aee69f649a51fb7e7108e
Paalpe <37175836+Paalpe@users.noreply.github.com>
2024-04-07 23:01:19 +0200

    PostCSS Vibes

--- src/main.js
+++
@@ -1,5 +0,0 @@
-import './app.css';
-
-document.querySelector('#app').innerHTML = `
-  <button class="btn">Hello daisyUI</button>
-`;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,34 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  content: ['./src/**/*.{vue,js,ts}'],
-  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#fca5a5",
+          secondary: "#370079",
+          accent: "#fde68a",
+          neutral: "#4f46e5",
+          "base-100": "#deffb7",
+          info: "#7dd3fc",
+          success: "#00f98d",
+          warning: "#ffb300",
+          error: "#f10028",
+        },
+      },
+    ],
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Merriweather", ...defaultTheme.fontFamily.sans],
+      },
+    },
+
+    container: {
+      center: true,
+    },
+  },
+  content: ["./public/*.html"],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
